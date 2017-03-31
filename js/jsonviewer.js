953b956ec5c3428df08cdf61ceaa0dcd6ae2273e
hrehman200 <hrehman200@gmail.com>
2017-04-01 00:21:08 +0500

    Working code

--- js/jsonviewer.js
+++ js/jsonviewer.js
@@ -2,6 +2,12 @@ $(function () {
 
     var html = "";
 
+    $.fn.overflown = function(){
+        var e = this[0];
+        var result = e.scrollHeight > e.clientHeight || e.scrollWidth > e.clientWidth;
+        return result;
+    };
+
     $.fn.extend({
         treed: function (o) {
 
@@ -14,7 +20,6 @@ $(function () {
             tree.find('li > ul').each(function () {
                 var branch = $(this).parent(); //li with children ul
                 branch.prepend("<i class='indicator glyphicon " + closedClass + "'></i>");
-                //branch.addClass('branch');
                 branch.on('click', function (e) {
                     if (this == e.target) {
                         var icon = $(this).children('i:first');
@@ -65,58 +70,97 @@ $(function () {
         $('#tree2').append(html)
             .treed({openedClass:'glyphicon-chevron-up', closedClass:'glyphicon-chevron-down'});
 
-        $('li').show();
+        //$('li').show();
+
+        $('.subTable .row').each(function() {
+            var divs = $(this).find('> div[class*="col-md-"]');
+            if(divs.length > 2) {
+                while(sumOfGridColumns(divs) < 12) {
+                    incrementGridColumns(divs);
+                }
+            }
+        });
+
+        $('.subTable .row > div[class*="col-md-"]').each(function() {
+             if($(this).overflown()) {
+                 console.log($(this).attr('class'));
+
+                 var previousWidth = Number($('.container-fluid').css('width').replace('px', ''));
+                 //$('.container-fluid').css('width', (previousWidth + 30) + 'px');
+             }
+        });
     });
+
+    function sumOfGridColumns(divs) {
+        var sum = 0;
+        for(var i=0; i<divs.length; i++) {
+            var div = $(divs[i]);
+            sum += getColumnClassNumber(div);
+        }
+        return sum;
+    }
+
+    function incrementGridColumns(divs) {
+        for(var i=divs.length; i>1; i--) {
+            var div = $(divs[i]);
+            var no = getColumnClassNumber(div);
+            no += 1;
+
+            div.attr('class', 'col-md-'+no);
+        }
+    }
+
+    function getColumnClassNumber(div) {
+        var className = div.attr('class');
+        if(typeof className == 'undefined') {
+            return 0;
+        }
+        if(className.indexOf(" ") !== -1) {
+            var arr = className.split(" ");
+            className = arr[0];
+        }
+        var no = className.split("-")[2];
+        return Number(no);
+    }
 
     function generateHtmlForJsonObj(key, jsonObj) {
 
         if($.isArray(jsonObj)) {
 
-            html += '<li class="branch"><a href="javascript:;">'+key+' '+'<span>('+jsonObj.length+')</span></a> \
-                <ul>';
+            html += '<li class="branch"><a href="javascript:;">'+key+' '+' ('+jsonObj.length+')</a> \
+                <ul class="subTable">';
 
             var jsonArray = jsonObj;
-
             var keysArray = getKeysFromJsonArray(jsonArray);
 
-            var headerColumns = "<div class='col-md-1'></div>";
-            var colClass = 0;
-            if(keysArray[0].length <=3) {
-                colClass = Math.floor(10 / (keysArray.length-1));
-            } else {
-                colClass = Math.floor(11 / (keysArray.length));
-            }
-
+            // append table header
+            var headerColumns = "<div class='col-md-1 yellowbg'>&nbsp;</div>";
             for(var i in keysArray) {
-                headerColumns += '<div class="col-md-'+(keysArray[i].length<=3 ? 1 : colClass)+'"><b><span class="brackets">{ } </span> '+keysArray[i]+'</b></div>';
+                headerColumns += '<div class="col-md-'+(keysArray[i].columnClass)+' nowrap"><b><span class="brackets">{ } </span> '+keysArray[i].key+'</b></div>';
             }
-            var header = '<li><div class="row yellowbg">'+ headerColumns + '</div></li>';
-
+            var header = '<li><div class="row row-eq-height yellowbg">'+ headerColumns + '</div></li>';
             html += header;
 
             for (var row=0; row<jsonArray.length; row++) {
                 html += '<li>' +
-                    '<div class="row">' +
+                    '<div class="row row-eq-height">' +
                         '<div class="col-md-1 yellowbg"><b>'+(row+1)+'</b></div>';
 
                 for(var col=0; col<keysArray.length; col++) {
 
-                    var arrayItem = jsonArray[row][keysArray[col]];
+                    var arrayItem = jsonArray[row][keysArray[col].key];
                     if(typeof arrayItem == 'object' || $.isArray(arrayItem)) {
 
-                        html += '<div class="col-md-'+colClass+'"> \
+                        html += '<div class="col-md-'+keysArray[col].columnClass+'"> \
                             <ul class="subTree table">';
 
-                        generateHtmlForJsonObj(keysArray[col], arrayItem, true);
+                        generateHtmlForJsonObj(keysArray[col].key, arrayItem);
 
                         html += '</ul> \
                             </div>';
 
                     } else {
-                        if(typeof  arrayItem == 'undefined') {
-                            arrayItem = '';
-                        }
-                        html += '<div class="col-md-'+(col==0 ? 1 : colClass)+'">' + arrayItem + '</div>';
+                        html += '<div class="col-md-'+(keysArray[col].columnClass)+'">' + cleanValue(arrayItem) + '</div>';
                     }
                 }
                 html += '</div> \
@@ -129,11 +173,11 @@ $(function () {
 
         } else if(typeof jsonObj == 'object') {
 
-            html += '<li class="branch"><a href="javascript:;">'+key+'</a> \
+            html += '<li class="branch nowrap"><a href="javascript:;">'+key+'</a> \
                 <ul>';
 
             for(var key2 in jsonObj) {
-                generateHtmlForJsonObj(key2, jsonObj[key2], false);
+                generateHtmlForJsonObj(key2, jsonObj[key2]);
             }
 
             html += '</ul> \
@@ -142,8 +186,8 @@ $(function () {
         } else {
             html += '<li> \
                 <div class="row"> \
-                    <div class="col-md-6"><b><span class="brackets">{ } </span> '+key+'</b></div> \
-                    <div class="col-md-6">'+jsonObj+'</div> \
+                    <div class="col-md-6 nowrap"><b><span class="brackets">{ } </span> '+key+'</b></div> \
+                    <div class="col-md-6 nowrap">'+cleanValue(jsonObj)+'</div> \
                 </div> \
             </li>';
         }
@@ -159,14 +203,62 @@ $(function () {
         var keysArray = [];
 
         for (var i=0; i<jsonArray.length; i++) {
+            var columnValueMaxLength = 0;
             for (var prop in jsonArray[i]) {
-                if (jsonArray[i].hasOwnProperty(prop) && keysArray.indexOf(prop) === -1) {
-                    keysArray.push(prop);
+
+                var arrExisted = $.grep(keysArray, function(item) {
+                    return item.key == prop;
+                });
+
+                if (jsonArray[i].hasOwnProperty(prop) && arrExisted.length == 0) {
+                    keysArray.push({key:prop, maxLength:0});
                 }
             }
         }
 
+        // based on max-length of column, we need to assign each column a css class col-md-*
+        var totalLength = 0;
+
+        for(var i in keysArray) {
+
+            var key = keysArray[i].key;
+            var columnValueMaxLength = 0;
+
+            for(var j in jsonArray) {
+
+                var value = typeof jsonArray[j][key] == 'undefined' ? '' : jsonArray[j][key];
+
+                if(columnValueMaxLength < key.length) {
+                    columnValueMaxLength = key.length;
+                }
+                if(columnValueMaxLength < value.length) {
+                    columnValueMaxLength = value.length;
+                }
+            }
+            keysArray[i].maxLength = columnValueMaxLength;
+            totalLength += columnValueMaxLength;
+        }
+
+        for(var i in keysArray) {
+            var percent = keysArray[i].maxLength / totalLength * 100;
+            var columnClass = Math.floor(percent * 11 / 100);
+            if(columnClass == 0 || keysArray[i].key == "@id" || keysArray[i].key == "id") {
+                columnClass = 1;
+            }
+            keysArray[i].columnClass = columnClass;
+        }
+
         return keysArray;
+    }
+
+    /**
+     * Replace undefined or empty values with whitespace character
+     *
+     * @param val
+     * @returns {string}
+     */
+    function cleanValue(val) {
+        return typeof(val) == 'undefined' || val == '' ? '&nbsp;' : val;
     }
 });
 
